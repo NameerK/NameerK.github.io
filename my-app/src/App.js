3eadd25e834a77ed9172e50ddeb79b31cf02d17f
NameerK <93886756+NameerK@users.noreply.github.com>
2024-06-12 16:37:43 -0400

    tic-tac-toe v1.0

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -38,9 +38,7 @@ function Square({value, onSquareClick}) { // function that creates each square i
   );
 }
 
-function Board() { // function that creates the board out of squares and keeps track of the state of the board
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+function Board({ xIsNext, squares, onPlay }) { // function that creates the board out of squares and keeps track of the state of the board
 
   function handleClick(i) { // function that handles the click of each square
     if (squares[i] || calculateWinner(squares)) {
@@ -52,8 +50,7 @@ function Board() { // function that creates the board out of squares and keeps t
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -87,13 +84,43 @@ function Board() { // function that creates the board out of squares and keeps t
 }
 
 function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
