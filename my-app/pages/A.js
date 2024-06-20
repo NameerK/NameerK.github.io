907e33e2d5ccc44602bc99505e0af64b3429a7cd
NameerK <93886756+NameerK@users.noreply.github.com>
2024-06-20 19:44:16 -0400

    navbar w/ other pages & hover effect
    added pages for testing as well as hover color change effect

---
+++ my-app/pages/A.js
@@ -0,0 +1,8 @@
+export default function A() {
+    return (
+        <div>
+            <h1>Page A</h1>
+            <p>This is page A</p>
+        </div>
+    )
+}
---
+++ my-app/pages/B.js
@@ -0,0 +1,8 @@
+export default function B() {
+    return (
+        <div>
+            <h1>Page B</h1>
+            <p>This is page B</p>
+        </div>
+    )
+}
